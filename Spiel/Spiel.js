3cf2012c7d9093fecdb2d3f598e2e94cbae474bb
Alex Opoku <alex.opoku@iteratec.de>
2017-11-09 16:48:40 +0100

    Den Switch-Case Fall hinzugefügt

--- Spiel/Spiel.js
+++ Spiel/Spiel.js
@@ -25,16 +25,69 @@ function bet1000(){
   money=money-1000;
   console.log(money);
 }
-
+function bet2000(){
+  money=money-2000;
+  console.log(money);
+}
+function bet3000(){
+  money=money-3000;
+  console.log(money);
+}
 function addCard(){
-card = Math.round(1+Math.random()*12);
-
-switch (card) {
+var c = Math.round(1+Math.random()*12);
+switch (c) {
   case 1:
-    img.src="./images/ass.png"
+    card=img.src="./images/ass.png"
+    points=points+card;
     break;
-  default:
-
+  case 2:
+  card = img.src="./images/zwei.png"
+    points=points+card;
+    break;
+  case 3:
+    card=img.src="./images/drei.png"
+    points=points+card;
+    break;
+  case 4:
+    card=img.src="./images/vier.png"
+    points=points+card;
+    break;
+  case 5:
+    card=img.src="./images/fünf.png"
+    points=points+card;
+    break;
+  case 6:
+    card=img.src="./images/sechs.png"
+    points=points+card;
+    break;
+  case 7:
+    card=img.src="./images/sieben.png"
+    points=points+card;
+    break;
+  case 8:
+    card=img.src="./images/acht.png"
+    points=points+card;
+    break;
+  case 9:
+    card=img.src="./images/neun.png"
+    points=points+card;
+    break;
+  case 10:
+    card=img.src="./images/zehn.png"
+    points=points+card;
+    break;
+  case 11:
+    card=img.src="./images/bube.png"
+    points=points+card;
+    break;
+  case 12:
+    card=img.src="./images/königin.png"
+    points=points+card;
+    break;
+  case 13:
+    card=img.src="./images/könig.jpg"
+    points=points+card;
+    break;
 }
 }
 
